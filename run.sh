a67de0f5ae185ed1e056a9a13cf2bd2168280a0f
tbauer516 <tcbauer516@gmail.com>
2017-11-13 13:59:59 -0800

    make container algo agnostic

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-source creds
-
-if [[ "$#" -eq 2 ]]; then
-  docker run -d --name magiminer -e UNAME=$UNAME -e WNAME=$WNAME -e WPASS=$WPASS -e THR=$1 -e TEMP=$2 magiminer-image
-elif [[ "$#" -eq 1 ]]; then
-  docker run -d --name magiminer -e UNAME=$UNAME -e WNAME=$WNAME -e WPASS=$WPASS -e THR=$1 magiminer-image
-else
-  echo "usage: run.sh <num threads> [<max temp>]"
+if [[ "$#" -lt 1 || "$#" -gt 3 ]]; then
+  echo "usage: run.sh <credential file> <num threads> [<max temp>]"
   exit 1
 fi
+
+source $1
+
+docker run -d ${NAME:+'--name' "$NAME"} -e ALGO=$ALGO -e POOLADDRESS=$POOLADDRESS -e UNAME=$UNAME -e WNAME=$WNAME -e WPASS=$WPASS -e THR=$2 ${3:+'-e' "TEMP=$3"} miner-image
